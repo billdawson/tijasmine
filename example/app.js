1b478073daf5da9883f6216c22cc1201179b3e96
billdawson <bill@billdawson.com>
2013-05-11 03:07:52 +0200

    Re example app: add the specs that make up the jasmine 1.3.1 intro page:
    http://pivotal.github.io/jasmine/

--- example/app.js
+++ example/app.js
@@ -1,6 +1,6 @@
 var tijasmine = require("/tijasmine/tijasmine"),
 	reporter = new (require("/tijasmine/tijasmine-console").ConsoleReporter)();
 
-tijasmine.addSpecModules("/specs/myspec", "/specs/myotherspec");
+tijasmine.addSpecModules("/specs/myspec", "/specs/myotherspec", "/specs/jasmineIntro");
 tijasmine.addReporter(reporter);
 tijasmine.execute();
